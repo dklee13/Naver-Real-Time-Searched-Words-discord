11589c08198b836c3cd1238a5103e639165c4527
dklee13 <71491713+dklee13@users.noreply.github.com>
2020-11-19 21:47:32 +0900

    Update ranks.py

--- ranks.py
+++ ranks.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 성공적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('실시간 검색어')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
@@ -30,4 +30,4 @@ async def on_message(message):
                 break
 
 
-client.run('★TOKEN★')
+client.run('Nzc4OTY0MTk1ODM1NzcyOTI4.X7Zo8A.TIJxfhJAsPBaSHSNToHtlx-ZKUg')
